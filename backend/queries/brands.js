63074a8cabf4e2bf31237443d5abdceab6423226
AminesCodes <aminebensalem@pursuit.org>
2020-02-23 10:17:36 -0500

    Backend - Users and Brands router

--- backend/queries/brands.js
+++ backend/queries/brands.js
@@ -1,32 +1,32 @@
 const db = require('../database/db')
 
-const getUserById = id => {
-    return await db.one('Select * from users where id = $1', id)
+const getBrandById = id => {
+    return await db.one('Select * from brands where id = $1', id)
 }
 
-const getUserByEmail = email => {
-    return await db.one('Select * from users where email = $1', email)
+const getBrandByEmail = email => {
+    return await db.one('Select * from brands where email = $1', email)
 }
 
-const createUser = (email, password, firstName, lastName) => {
+const createBrand = (email, password, name, businessID) => {
     const insertQuery = `
-        INSERT INTO users
-            (email, password, firstname, lastname)
+        INSERT INTO brands
+            (email, password, name, business_id)
         VALUES
             ($1, $2, $3, $4) 
         RETURNING *
     `
-    return await db.one(insertQuery, [email, password, firstName, lastName])
+    return await db.one(insertQuery, [email, password, name, businessID])
 }
 
-const updateUserInfo = (id, email, firstName, lastName  ) => {
+const updateBrandInfo = (id, email, name, businessID) => {
     const updateQuery = `
-        UPDATE users 
-        SET email=$2, firstname=$3, lastname=$4
+        UPDATE brands 
+        SET email=$2, name=$3, business_id=$4
         WHERE id = $1 
         RETURNING *
     `
-    return await db.one(updateQuery, [id, email, firstName, lastName])
+    return await db.one(updateQuery, [id, email, name, businessID])
 }
 
 const updateBrandPassword = (id, password) => {
