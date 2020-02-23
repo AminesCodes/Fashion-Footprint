edb841aad2b45c721599d744a2acd867919c2778
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2020-02-23 13:31:27 -0500

    seeded random data into the database

--- backend/seed.sql
+++ backend/seed.sql
@@ -1,25 +1,24 @@
 
-DROP DATABASE IF EXISTS hackathon_db;
+DROP DATABASE IF EXISTS fashion_footprint_db;
 
-CREATE DATABASE hackathon_db;
+CREATE DATABASE fashion_footprint_db;
 
-\c hackathon_db;
+\c fashion_footprint_db;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR,
-    email VARCHAR UNIQUE,
-    password VARCHAR,
-    firstname VARCHAR, 
-    lastname VARCHAR
+    email VARCHAR UNIQUE NOT NULL,
+    password VARCHAR NOT NULL,
+    firstname VARCHAR NOT NULL, 
+    lastname VARCHAR NOT NULL
 );
 
 CREATE TABLE brands (
     id SERIAL PRIMARY KEY,
-    email VARCHAR UNIQUE,
-    password VARCHAR,
-    name VARCHAR,
-    businessId INT UNIQUE
+    email VARCHAR UNIQUE NOT NULL,
+    password VARCHAR NOT NULL,
+    name VARCHAR NOT NULL,
+    businessId INT UNIQUE NOT NULL
 );
 
 CREATE TABLE textiles (
@@ -30,6 +29,11 @@ CREATE TABLE textiles (
   environmentalImpact VARCHAR
 );
 
+CREATE TABLE styles  (
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR
+);
+
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     brand_id INT REFERENCES brands(id) ON DELETE CASCADE,
@@ -37,21 +41,12 @@ CREATE TABLE products (
     name VARCHAR,
     defaultPic VARCHAR,
     description VARCHAR,
-    specifications VARCHAR,
     closingDate DATE, 
+    style INT REFERENCES styles(id),
     textile_id INT REFERENCES textiles(id)
 );
 
-CREATE TABLE styles  (
-  id SERIAL PRIMARY KEY, 
-  name VARCHAR,
-  pic VARCHAR,
-  product_id  INT  REFERENCES products(id)
-);
-
-
-
-CREATE TABLE wishlist (
+CREATE TABLE wishlists (
   id SERIAL PRIMARY KEY,
   willingToBuy BOOLEAN,
   user_id INT REFERENCES users(id),
@@ -64,13 +59,30 @@ CREATE TABLE facts (
   fact VARCHAR
 );
 
-INSERT INTO users (username, email, password, firstname, lastname)
-    VALUES ('kingjames', 'lebronjames@gmail.com', 'lakersforlife', 'lebron', 'james'),
-            ('inmyfeelings', 'drake@jumpman.com', 'godsplan', 'Aubrey', 'Graham');
+INSERT INTO users (email, password, firstname, lastname)
+    VALUES ('lebronjames@gmail.com', 'lakersforlife', 'lebron', 'james'),
+            ('drake@jumpman.com', 'godsplan', 'Aubrey', 'Graham');
 
 INSERT INTO brands (email, password, name, businessId)
     VALUES ('Uber@gmail.com', 'UberEats', 'Uber', 9245632),
            ('Nba@gmail.com', 'leberon', 'Espn', 272774721);
+
+INSERT INTO textiles (name, pic, care, environmentalImpact)
+    VALUeS ('wool', 'http://localhost:3030/textiles/wool.jpg', 'You shoulc care for wool', 'wool is natural because sheep'),
+          ('polyester', 'http://localhost:3030/textiles/polyester.jpg', 'You shouldnt care for polyester', 'bad for environment, bad for you');
+
+INSERT INTO styles (name)
+    VALUES ('red stuff');
+
+INSERT INTO  products (brand_id, type, name, defaultPic, description, closingDate, style, textile_id)
+    VALUES (1, 'shirt', 'red shirt', 'http://localhost:3030/images/redshirt.jpg', 'This is a red shirt', '2008-11-11', 1, 1 ),
+            (2, 'pants', 'red pants', 'http://localhost:3030/images/redpants.jpg', 'These are red pants', '2009-11-11', 1, 2);
+
+INSERT INTO wishlists (willingToBuy, user_id, product_id) 
+    VALUES (true, 1, 2), (false, 2, 1);
+
+INSERT INTO facts (fact)
+    VALUES ('Your use of this site helps the environment 100%'), ('This is amazing');
 
 
              
