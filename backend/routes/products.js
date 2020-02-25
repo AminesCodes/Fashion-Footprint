54c00c0c6dd3aaaf81278169a9a16fc1f6db1908
Suzette <suzetteislam@gmail.com>
2020-02-24 20:10:14 -0500

    editing the products router

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const productQueries = require('../queries/products');
-const {handleErrors} = require('./helpers/helpers');
+const { handleErrors } = require('./helpers/helpers');
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: (request, file, cb) => {
+        cb(null, './public/products')
+    },
+    filename: (request, file, cb) => {
+        const filename = Date.now() + '-' + file.originalname
+        cb(null, filename)
+    }
+
+})
+
+const fileFilter = (request, file, cb) => {
+    if (file.mimetype.slice(0, 6) === 'image/') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
+const upload = multer({
+    storage, fileFilter
+})
 
 /* GET all product by Brand Id */
 router.get('/:brand_id/all', async (req, res, next) => {
@@ -21,29 +45,29 @@ router.get('/:brand_id/all', async (req, res, next) => {
 })
 
 /* GET all product by type Id */
-router.get('/:type_id', async (req, res) =>{
+router.get('/type/:type_id', async (req, res) => {
     let type_id = req.params.type_id
 
-    try{
+    try {
         let productByType = await productQueries.getProductByType(type_id)
         res.status(200).json({
-            message: `Success, retrieved all products by type with id #${type_id}`, 
+            message: `Success, retrieved all products by type with id #${type_id}`,
             payload: productByType
         })
 
-    } catch(err) {
+    } catch (err) {
         handleErrors(res, err)
     }
 })
 
 /* GET all product by textile Id */
-router.get('/:textile_id', async(req, res) => {
+router.get('/material/:textile_id', async (req, res) => {
     let material_id = req.params.textile_id
 
-    try{
+    try {
         let productByMaterial = await productQueries.getProductByMaterial(material_id)
         res.status(200).json({
-            message: `Success, retrieved all products by material with id #${material_id}`, 
+            message: `Success, retrieved all products by material with id #${material_id}`,
             payload: productByMaterial
         })
 
@@ -54,7 +78,7 @@ router.get('/:textile_id', async(req, res) => {
 
 
 /* POST new product*/
-router.post('/add/:brand_id', async(req, res) =>{
+router.post('/add/:brand_id', upload.single('product_pic'), async (req, res) => {
 
     let brand_id = req.params.brand_id
     let type_id = req.body.type_id
@@ -66,16 +90,16 @@ router.post('/add/:brand_id', async(req, res) =>{
 
     if (type_id, name, default_pic, description, closing_date, brand_id) {
 
-    try {
-        let newProduct = await productQueries.createProduct(type_id, name, default_pic, description, closing_date, brand_id)
-        res.status(200).json({
-            message: "Success added new product",
-            payload: newProduct
-        })
+        try {
+            let newProduct = await productQueries.createProduct(type_id, name, default_pic, description, closing_date, brand_id)
+            res.status(200).json({
+                message: "Success added new product",
+                payload: newProduct
+            })
 
-    } catch (err) {
-        handleErrors(res, err)
-    }
+        } catch (err) {
+            handleErrors(res, err)
+        }
 
     } else {
         res.status(403).json({
@@ -85,8 +109,8 @@ router.post('/add/:brand_id', async(req, res) =>{
 })
 
 
-/* PATCH single product */
-router.patch('/:product_id', async (req, res, next) => {
+/* PUT single product (updates everything) */
+router.put('/:product_id', upload.single('product_pic'), async (req, res, next) => {
 
     let product_id = req.params.product_id
     let type_id = req.body.type_id
