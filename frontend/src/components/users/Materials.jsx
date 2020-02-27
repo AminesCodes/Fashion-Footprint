27b7b4027740cc83fd89a62fb5919c8016820a0e
tbriany <brianytaveras@pursuit.org>
2020-02-27 18:50:45 -0500

    removed commented out Carousel items from materials component

--- frontend/src/components/users/Materials.jsx
+++ frontend/src/components/users/Materials.jsx
@@ -64,45 +64,6 @@ class Materials extends Component {
                     <Carousel>
                         {mappedMaterials}
                     </Carousel>
-                    {/* <Carousel>
-                        <Carousel.Item>
-                            <Card className="d-block w-100" style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="https://memoryfoammattress.org/wp-content/uploads/2017/04/Cotton-An-Ancient-and-Modern-Fiber-1024x576.jpg" />
-                                <Card.Body>
-                                    <Card.Title>
-                                        Cotton
-                                    </Card.Title>
-                                    <Card.Text >
-                                        This is some information about cotton blah blah blah
-                        </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Carousel.Item>
-
-                        <Carousel.Item>
-                            <Card className="d-block w-100" style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="https://cf.ltkcdn.net/fashion-history/images/std/212077-675x450-Polyester-fabric.jpg" />
-                                <Card.Body>
-                                    <Card.Title>Polyester</Card.Title>
-                                    <Card.Text>
-                                        This is some information about Polyester blah blah blah
-                        </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Carousel.Item>
-
-                        <Carousel.Item>
-                            <Card className="d-block w-100" style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="https://sc02.alicdn.com/kf/HTB1C.p.mb_I8KJjy1Xaq6zsxpXas/pure-silk-fabric-100-natural-silk-satin.jpg_350x350.jpg" />
-                                <Card.Body>
-                                    <Card.Title>Silk</Card.Title>
-                                    <Card.Text>
-                                        This is some information about Silk blah blah blah
-                        </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Carousel.Item>
-                    </Carousel> */}
                 </div>
             )
         }
