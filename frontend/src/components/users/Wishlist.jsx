0964bea0079a0503c6559299bb2f8bfc88ddca6d
Chuck Okonkwo <chukwukaaguolu@gmail.com>
2020-02-27 18:46:38 -0500

    adding all changes i have made so far to see the most up to date app

--- frontend/src/components/users/Wishlist.jsx
+++ frontend/src/components/users/Wishlist.jsx
@@ -4,17 +4,35 @@ import '../../index.css';
 
 
 
-const Wishlist = (props) => {
+class Wishlist extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			userBought: false
+		}
+	};
+
+	handleClick = async () => {
+	
+		
+		this.setState({userBought: !(this.state.userBought)});
+	}
+
+
+
+	render(){
+		let button = this.state.userBought ? <a href="#" className="btn btn-danger red" onClick={this.handleClick}>Bought</a> : <a href="#" className="btn btn-primary" onClick={this.handleClick}>Would Buy</a>
 	return (
 		<div className="card col-sm-3 wishlist-card mx-3">
- 			<img src={props.wishlist.default_pic} className="card-img-top " alt={props.wishlist.name} style={{height: '200px', objectFit: 'scale-down'}}/>
+ 			<img src={this.props.wishlist.default_pic} className="card-img-top " alt={this.props.wishlist.name} style={{height: '200px', objectFit: 'scale-down'}}/>
 	  		<div className="card-body">
-		    	<h5 className="card-title">{props.wishlist.name}</h5>
+		    	<h5 className="card-title">{this.props.wishlist.name}</h5>
 		    	<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-		    	<a href="#" className="btn btn-primary">Buy this!</a>
+		    	{button}
 	 		</div>
 		</div>
 	)
+	}
 }
 
 export default Wishlist;
