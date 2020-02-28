1c17066559b6b6bb219ce431dad4a70c9cb4d847
Amine Bensalem <aminebensalem@pursuit.org>
2020-02-27 23:06:41 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
@@ -22,7 +21,6 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
