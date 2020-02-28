cad4ee4949d346c24ab35588e74f225345463cd8
Amine Bensalem <aminebensalem@pursuit.org>
2020-02-28 00:06:45 -0500

    Update wishlist.js

--- backend/queries/wishlist.js
+++ backend/queries/wishlist.js
@@ -19,31 +19,33 @@ const updateWishlistItem = async (id) => {
     const updateQuery = `
         UPDATE wishlists
         SET willing_to_buy = NOT willing_to_buy
-        WHERE wishlist_id = $1 
-        RETURNING wishlists.willing_to_buy
-
+        WHERE id=$1 
+        RETURNING *
     `
     return await db.one(updateQuery, [id])
 }
 
 const createVote = async (id) =>  {
     const createdVote = `
-     INSERT INTO votes (product_vote_id, user_vote_id)
-        VALUES ((SELECT product_id FROM wishlists WHERE wishlist_id = $1), (SELECT user_id FROM wishlists WHERE wishlist_id = $1))
+     INSERT INTO votes (product_id, user_id)
+        VALUES ((SELECT product_id FROM wishlists WHERE id=$1), (SELECT user_id FROM wishlists WHERE id=$1))
         RETURNING *
     `
-    return await db.any(createdVote, [id]);
+    return await db.one(createdVote, [id]);
 }
 
 const deleteVote = async (id) => {
     const deletedVote = `
-
-    DELETE FROM votes WHERE product_vote_id = (SELECT product_id FROM wishlists WHERE wishlist_id = $1) AND user_vote_id = (SELECT user_id FROM wishlists WHERE wishlist_id = $1) `;
-    return await db.none(deletedVote, [id]);
+    DELETE FROM votes 
+    WHERE product_id = (SELECT product_id FROM wishlists WHERE id=$1) 
+    AND user_id = (SELECT user_id FROM wishlists WHERE id=$1) 
+    RETURNING *
+`;
+    return await db.one(deletedVote, [id]);
 };
 
 const deleteWishlistItem = async (id) => {
-    return db.one('DELETE FROM wishlist WHERE wishlist_id = $1 RETURNING *', id)
+    return db.one('DELETE FROM wishlist WHERE id=$1 RETURNING *', id)
 
 }
 
