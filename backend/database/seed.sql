5a0108049f8a4138b9a463e5e8d3f6bf1fe19d65
Amine Bensalem <aminebensalem@pursuit.org>
2020-02-27 23:17:32 -0500

    Update seed.sql

--- backend/database/seed.sql
+++ backend/database/seed.sql
@@ -24,7 +24,7 @@ CREATE TABLE brands (
 
 CREATE TABLE textiles (
   id SERIAL PRIMARY KEY,
-  textile_name VARCHAR,
+  name VARCHAR,
   pic VARCHAR,
   care VARCHAR,
   environmental_impact VARCHAR
@@ -44,28 +44,26 @@ CREATE TABLE styles  (
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     brand_id INT REFERENCES brands(id) ON DELETE CASCADE,
-
-    type_id INT REFERENCES types(id) ON DELETE CASCADE,
+    type_id INT REFERENCES types(id),
     name VARCHAR,
     default_pic VARCHAR,
     description VARCHAR,
     closing_date DATE, 
-    textile_id INT REFERENCES textiles(id) ON DELETE CASCADE,
+    textile_id INT REFERENCES textiles(id),
     going_to_production BOOlEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE votes  (
-
-    product_vote_id INT REFERENCES products(id) ON DELETE CASCADE,
-    user_vote_id INT REFERENCES users(id) ON DELETE CASCADE
+    product_id INT REFERENCES products(id) ON DELETE CASCADE,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE wishlists (
-  wishlist_id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   willing_to_buy BOOLEAN DEFAULT false,
-  user_id INT REFERENCES users(id) ON DELETE CASCADE,
-  product_id INT  REFERENCES products(id) ON DELETE CASCADE,
-  style_id INT REFERENCES styles(id) ON DELETE CASCADE
+  user_id INT REFERENCES users(id),
+  product_id INT  REFERENCES products(id),
+  style_id INT REFERENCES styles(id)
 );
 
 CREATE TABLE facts (
@@ -177,7 +175,7 @@ INSERT INTO brands (email, password, name, business_id)
 ('info@cavalli.com', '$2b$12$BnlkuACZiHUs8h0TLWejv.NaSyBXQGNWnczdYt8KrdDEDV9VHQ4/O', 'Cavalli', 1234664),
 ('info@brunello cucinelli.com', '$2b$12$BnlkuACZiHUs8h0TLWejv.NaSyBXQGNWnczdYt8KrdDEDV9VHQ4/O', 'Brunello Cucinelli', 1234665);
 
-INSERT INTO textiles (textile_name, pic, care, environmental_impact)
+INSERT INTO textiles (name, pic, care, environmental_impact)
     VALUES ('wool', '/images/textiles/wool.jpg', 'You shoulc care for wool', 'wool is natural because sheep'),
           ('polyester', '/images/textiles/polyester.jpg', 'You shouldnt care for polyester', 'bad for environment, bad for you');
 
@@ -278,7 +276,7 @@ INSERT INTO types (name)
            ('Women Tops'),
            ('Women Watches');
 
-INSERT INTO  products (brand_id, type_id, product_name, default_pic, description, closing_date, textile_id)
+INSERT INTO  products (brand_id, type_id, name, default_pic, description, closing_date, textile_id)
     VALUES (1, 1, 'red shirt', '/images/products/redshirt.jpg', 'This is a red shirt', '2008-11-11', 1 ),
             (2, 2, 'red pants', '/images/products/redpants.jpg', 'These are red pants', '2009-11-11', 2);
 
@@ -287,9 +285,3 @@ INSERT INTO wishlists (willing_to_buy, user_id, product_id)
 
 INSERT INTO facts (fact)
     VALUES ('Your use of this site helps the environment 100%'), ('This is amazing');
-
-
-INSERT INTO votes (product_vote_id, user_vote_id)
-  VALUES (1, 1), (1, 2), (2, 1), (2, 2), (2, 3), (2, 4);
-             
-     
