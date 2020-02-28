13d8a778d2735b5a9ccc14ec253ce2ecf2198713
AminesCodes <aminebensalem@pursuit.org>
2020-02-27 21:25:49 -0500

    Backend - votes query to include users emails

--- backend/queries/votes.js
+++ backend/queries/votes.js
@@ -1,7 +1,7 @@
 const db = require('../database/db')
 
 const getAllVotesByProduct = async (ProductId) => {
-    return await db.any('SELECT * FROM votes WHERE product_id=$1', ProductId);
+    return await db.any('SELECT product_id, email FROM votes JOIN users ON user_id=users.id WHERE product_id=$1', ProductId);
 }
 
 
