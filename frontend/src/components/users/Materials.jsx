844ded3577051f06507dcf7b09f570dba6ae3002
Amine Bensalem <aminebensalem@pursuit.org>
2020-02-29 14:03:11 -0500

    Update Materials.jsx

--- frontend/src/components/users/Materials.jsx
+++ frontend/src/components/users/Materials.jsx
@@ -49,8 +49,7 @@ class Materials extends PureComponent {
                         className="d-block w-100"
                         src={el.pic}
                         alt="First slide"
-                        // style={{ height: "90vh", width: "80vw", objectFit: 'scale-down'}}
-                        style={{height: "700px"}}
+                        style={{ height: "90vh", width: "80vw", objectFit: 'cover'}}
                     />
                     <Carousel.Caption>
                         <div className='' style={{ backgroundColor: "#DCDCDC", opacity: "0.8", borderRadius: "25px", height: '80%'}}>
